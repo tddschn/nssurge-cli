61eeef71848e3b4db261499f8d91dd799a9a4c9b
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-05-15 18:59:17 +0800

    add complete_policy_for_policy_group

--- nssurge_cli/group_commands.py
+++ nssurge_cli/group_commands.py
@@ -33,6 +33,7 @@ from nssurge_api.types import (
 import typer
 import asyncio
 from aiohttp import ClientSession, ClientResponse
+from .policy_commands import complete_policy, complete_proxy, complete_policy_and_proxy, complete_policy_for_policy_group
 
 app = typer.Typer(name="group")
 
@@ -42,24 +43,35 @@ async def get_policy_group(policy_group: PolicyGroup = typer.Argument(None)) -> 
     Get all policy groups, or a specific one.
     """
     async with SurgeAPIClient(*get_config()) as client:
-        return await (await client.get_policy_group(policy_group)).json()
+        try:
+            return await (await client.get_policy_group(policy_group)).json()
+        except Exception as e:
+            typer.secho(f"Error: {e}", err=True)
+            typer.secho('Using proxy as an arg causes Surge for Mac to crash as of version 4.5.0', err=True, fg='red')
+            return {}
 
 
 # @app.command('group')
-@app.callback(invoke_without_command=True)
-def policy_group(ctx: typer.Context,
-    policy_group: PolicyGroup,
-    output_json: bool = typer.Option(False, "--json", "-j"),
-    pretty_print: bool = typer.Option(False, "--pretty", "-p"),
-    rich_print: bool = typer.Option(False, "--rich", "-r"),
+# @app.callback(invoke_without_command=True)
+@app.command('policy')
+def policy_group(# ctx: typer.Context,
+    policy_group: PolicyGroup = typer.Argument(..., help="Policy group name", autocompletion=complete_policy),
+    # output_json: bool = typer.Option(False, "--json", "-j"),
+    # pretty_print: bool = typer.Option(False, "--pretty", "-p"),
+    # rich_print: bool = typer.Option(False, "--rich", "-r"),
 ):
     """
     Get all policy groups, or a specific one.
     """
-    if ctx.invoked_subcommand is not None:
-        return
+    # if ctx.invoked_subcommand is not None:
+    #     return
     policy_group_dict = asyncio.run(get_policy_group(policy_group))
-    typer_output_dict(policy_group_dict, output_json, pretty_print, rich_print)
+    # typer_output_dict(policy_group_dict, output_json, pretty_print, rich_print)
+    policy = policy_group_dict['policy']
+    if policy is None:
+        typer.echo(f"Policy group {policy_group} not found")
+    else:
+        typer.secho(f'The policy for group {policy_group} is {policy}')
 
 
 async def get_policy_group_test_results() -> dict:
@@ -92,11 +104,11 @@ async def select_group(policy_group: PolicyGroup, policy: Policy):
 
 
 @app.command("select")
-def select_group_command(policy_group: PolicyGroup, policy: Policy):
+def select_group_command(policy: PolicyGroup = typer.Argument(..., help='Policy group name', autocompletion=complete_policy), policy_selected: Policy = typer.Argument(..., help='Policy name', autocompletion=complete_policy_for_policy_group)):
     """
     Select a policy group.
     """
-    asyncio.run(select_group(policy_group, policy))
+    asyncio.run(select_group(policy, policy_selected))
 
 
 async def test_policy_group(policy_group: PolicyGroup) -> dict:
@@ -110,7 +122,7 @@ async def test_policy_group(policy_group: PolicyGroup) -> dict:
 
 @app.command("test")
 def test_policy_group_command(
-    policy_group: PolicyGroup,
+    policy_group: PolicyGroup = typer.Argument(..., help="Policy group name", autocompletion=complete_policy),
     output_json: bool = typer.Option(False, "--json", "-j"),
     pretty_print: bool = typer.Option(False, "--pretty", "-p"),
     rich_print: bool = typer.Option(False, "--rich", "-r"),
@@ -120,3 +132,7 @@ def test_policy_group_command(
     """
     test_dict = asyncio.run(test_policy_group(policy_group))
     typer_output_dict(test_dict, output_json, pretty_print, rich_print)
+
+@app.callback()
+def group_command_callback():
+    """For policy groups"""
--- nssurge_cli/policy_commands.py
+++ nssurge_cli/policy_commands.py
@@ -22,22 +22,13 @@ from aiohttp import ClientSession, ClientResponse
 app = typer.Typer(name="policy")
 
 @cache
-async def get_policy(policy: Policy | None = None) -> Policies | dict:
+async def get_policy(policy: Policy | None = None) -> dict:
     """
     Get all policies.
     """
     async with SurgeAPIClient(*get_config()) as client:
         policy_dict = await (await client.get_policy(policy)).json()
-        if policy is None:
-            return policy_dict
-        else:
-            if "error" in policy:
-                typer.secho(
-                    f'Failed to get policy {policy}: {policy_dict["error"]}',
-                    fg=typer.colors.RED,
-                )
-                raise typer.Exit(1)
-            return policy_dict
+        return policy_dict
 
 def complete_policy(incomplete: str) -> Iterable[tuple[str, str]]:
     """
@@ -52,6 +43,49 @@ def complete_policy(incomplete: str) -> Iterable[tuple[str, str]]:
     
     return p2type_mapping.items()
 
+def complete_proxy(incomplete: str) -> Iterable[tuple[str, str]]:
+    """
+    Complete policy names.
+    """
+    incomplete = incomplete.lower()
+    policy_dict: Policies = asyncio.run(get_policy()) # type: ignore
+    proxies: Proxies = policy_dict["proxies"]
+    # policy_groups: PolicyGroups = policy_dict["policy-groups"]
+    # p2type_mapping = {p: 'policy group' for p in policy_groups if incomplete in p.lower()}
+    p2type_mapping = {p: 'proxy' for p in proxies if incomplete in p.lower()}
+    
+    return p2type_mapping.items()
+
+def complete_policy_and_proxy(incomplete: str) -> Iterable[tuple[str, str]]:
+    """
+    Complete policy names.
+    """
+    incomplete = incomplete.lower()
+    policy_dict: Policies = asyncio.run(get_policy()) # type: ignore
+    proxies: Proxies = policy_dict["proxies"]
+    policy_groups: PolicyGroups = policy_dict["policy-groups"]
+    p2type_mapping = {p: 'policy group' for p in policy_groups if incomplete in p.lower()}
+    p2type_mapping.update({p: 'proxy' for p in proxies if incomplete in p.lower()})
+    
+    return p2type_mapping.items()
+
+def complete_policy_for_policy_group(ctx: typer.Context, incomplete: str) -> Iterable[str]:
+    """
+    Complete policy names.
+    """
+    policy = ctx.params.get('policy')
+
+    # like 'Netflix = select, Proxy, Direct'
+    try:
+        policy_rule_for_group: str = asyncio.run(get_policy(policy=policy))[policy]
+    except:
+        typer.secho(f'No policy found for policy group {policy}', fg='red', err=True)
+        return []
+    policies_str = policy_rule_for_group.split('=')[1].strip()
+    import re
+    policies: list[str] = re.split(r'\s*,\s*', policies_str)
+
+    return [p for p in policies if incomplete.lower() in p.lower()]
 
 # @app.command("policy")
 @app.callback(invoke_without_command=True)
@@ -67,5 +101,16 @@ def policy(ctx: typer.Context,
     if ctx.invoked_subcommand is not None:
         return
     policy_dict = asyncio.run(get_policy(policy))
+    if policy is None:
+        # return policy_dict
+        pass
+    else:
+        if "error" in policy_dict:
+            typer.secho(
+                f'Failed to get policy {policy}: {policy_dict["error"]}',
+                fg=typer.colors.RED,
+            )
+            raise typer.Exit(1)
+        # return policy_dict
     typer_output_dict(policy_dict, output_json, pretty_print, rich_print)  # type: ignore
 
